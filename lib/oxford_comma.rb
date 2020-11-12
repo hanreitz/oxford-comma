caaadf4f860e5e1a0224b42c85843c4dfd614d2b
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-12 16:15:52 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size <= 1
+        array.each {|thing| return array.join}
+    elsif array.size == 2
+        array.each {|thing| return array.join(" and ")}
+    else 
+        new_array = []
+        new_array << array.slice!(0..array.size - 2)
+        new_array << "and #{array[-1]}"
+        new_array.each {|thing| return new_array.join(", ")}
+    end
 end
